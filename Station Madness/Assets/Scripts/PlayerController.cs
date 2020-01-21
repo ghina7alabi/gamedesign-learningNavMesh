d0036c9c29c5520ae08dc42e690ede90b7c7eeb5
Gina Al Halabi <ghina7alabi@gmail.com>
2020-01-21 18:46:25 +0300

    Added Files

---
+++ Station Madness/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject[] Targets;
+    public GameObject SelectedTarget;
+    public NavMeshAgent Agent;
+
+	// Use this for initialization
+	void Start () {
+        SelectTarget();
+        Agent.SetDestination(SelectedTarget.transform.position);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SelectTarget()
+    {
+        SelectedTarget = Targets[Random.Range(0, 4)];
+    }
+}
---
+++ Station Madness/Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public int SpawnCount = 20;
+    public GameObject[] Chars;
+
+	// Use this for initialization
+	void Start () {
+        CharSpawner();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void CharSpawner()
+    {
+        for (int i = 0; i < SpawnCount; i++)
+        {
+            Instantiate(Chars[Random.Range(0, 4)], this.gameObject.transform.position, Quaternion.identity);
+        }
+    }
+}
